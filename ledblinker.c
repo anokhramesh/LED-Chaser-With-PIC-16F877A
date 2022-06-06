968db572067248bd8a49eaf04c39a50ed319f6ff
Anokhautomation <67797819+anokhramesh@users.noreply.github.com>
2022-06-06 14:53:07 +0400

    Add files via upload

---
+++ ledblinker.c
@@ -0,0 +1,22 @@
+/*
+ * File:   ledblinker.c
+ * Author: user
+ *
+ * Created on June 6, 2022, 12:44 PM
+ */
+#include<stdio.h>
+#include<stdlib.h>
+#include <xc.h>
+#define _XTAL_FREQ 2000000
+
+__CONFIG(FOSC_HS & WDTE_OFF & PWRTE_OFF & BOREN_OFF & LVP_ON & CPD_OFF & WRT_OFF & CP_OFF);
+
+void main() {
+    TRISB = 0x00;//set port B as an out put
+    while(1){
+        PORTB =0xff;// turn High all pins in PORT B
+        __delay_ms(500);// Delay 500 milli second
+        PORTB =0x00;// turn Low all pins in Port B
+        __delay_ms(500);//delay 500 milli second
+    }
+}
